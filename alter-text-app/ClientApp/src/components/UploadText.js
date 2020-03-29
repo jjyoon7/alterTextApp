5fb79b7efbe09e531369a291c7764ce14aeb62e1
jjyoon7 <jay.yoon7@gmail.com>
2020-03-29 21:38:59 +0200

    basic setup

---
+++ alter-text-app/ClientApp/src/components/UploadText.js
@@ -0,0 +1,48 @@
+﻿import React, { useState } from "react"
+import axios from 'axios';
+
+export default function UploadText() {
+    const [hasSelectedFile, setHasSelectedFile] = useState(false)
+    const [fileName, setFileName] = useState("")
+
+
+    const onChangeHandler = (event) => {
+        const chosenFile = event.target.files[0]
+        const hasProperty = chosenFile.name !== ""
+        if (hasProperty) {
+            setHasSelectedFile(true)
+            setFileName(chosenFile.name)
+        }
+    }
+
+    const onClickHandler = (event) => {
+        const file = event.target.files[0];
+        console.log(file);
+        const formData = new FormData();
+        console.log(axios)
+        //formData.append("file", file);
+        //formData.append("__RequestVerificationToken", "@Context.GetAntiforgeryToken()")
+
+        //axios.post("/FileUpload/SingleFile", formData);
+    }
+
+    return (
+        <div className="file-upload-display">
+            <form className="file-upload-display-form" asp-controller="Home" asp-action="Index"
+                encType="multipart/form-data" method="post">
+                <label className="file-upload-display-label btn-file-upload-display-label btn">
+                    <input type="file" name="file" />
+                    <span className="file-upload-display-btn-text">{hasSelectedFile ? fileName : "Select a file"}</span>
+                </label>
+                <input className="file-upload-display-upload btn-file-upload-display-upload btn" type="submit" value="Upload" />
+            </form>
+
+            <form encType="multipart/form-data" method="post">
+                <label asp-for="FileUpload.FormFile"></label>
+                <input asp-for="FileUpload.FormFile" type="file"></input>
+                <span asp-validation-for="FileUpload.FormFile"></span>
+                <input asp-page-handler="Upload" className="btn" type="submit" value="Upload"></input>
+            </form>
+        </div>
+    )
+}
---
+++ alter-text-app/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace alter_text_app.Controllers
+{
+    [Route("api/Home")]
+    public class ValuesController : Controller
+    {
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            // GET: api/values
+
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            //Create a unique name for the container
+            string containerName = "filedata" + Guid.NewGuid().ToString();
+
+            // Create the container and return a container client object
+            BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+            // Create a local file in the ./data/ directory for uploading and downloading
+            string localPath = "./data/";
+            string fileName = "textfile" + Guid.NewGuid().ToString() + ".txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+
+
+            // Get a reference to a blob
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+            // Open the file and upload its data
+            using FileStream uploadFileStream = File.OpenRead(localFilePath);
+            await blobClient.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+
+
+            // Download the blob to a local file
+            // Append the string "DOWNLOAD" before the .txt extension 
+            // so you can compare the files in the data directory
+            string downloadFilePath = localFilePath.Replace(".txt", "DOWNLOAD.txt");
+
+            Console.WriteLine("\nDownloading blob to\n\t{0}\n", downloadFilePath);
+
+            // Download the blob's contents and save it to a file
+            BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+            using (FileStream downloadFileStream = File.OpenWrite(downloadFilePath))
+            {
+                await download.Content.CopyToAsync(downloadFileStream);
+                downloadFileStream.Close();
+            }
+
+            [HttpGet]
+            public IEnumerable<string> Get()
+            {
+                return new string[] { "value1", "value2" };
+            }
+
+            // POST api/values
+            [HttpPost]
+            public void Post([FromBody]string value)
+            {
+            }
+
+            // PUT api/values/5
+            [HttpPut("{id}")]
+            public void Put(int id, [FromBody]string value)
+            {
+            }
+    
+    }
+}
+
